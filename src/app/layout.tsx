9b865734a42067266ff106f02a517dfa66cbcc4a
LizzieH97 <erhughes97@gmail.com#>
2025-07-25 12:35:11 +0100

    fixed background issues?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`
           ${schoolbell.variable}
-          antialiased
+          antialiased bg-bark
         `}
       >
         <div
